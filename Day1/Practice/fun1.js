66faf5619b51c58d05044cfd8f0ae95577bc865e
Harshali Patil <harshali.patil@mindbowser.com>
2026-03-16 19:28:22 +0530

    Day 1 Assignment and Practice Code

---
+++ Day1/Practice/fun1.js
@@ -0,0 +1,20 @@
+const names='Harshali';
+const age=22;
+
+function display(username,userage){
+
+    return  `Name is ${username} and age is ${userage}`;
+      //  'Name is '+username+'Age is'+userage
+     
+    
+}
+
+console.log(display(names,age));
+
+
+const add=(num1,num2)=> {return num1+num2};
+console.log(add(10,20));
+
+const info=()=>{
+    console.log("hello")
+}
