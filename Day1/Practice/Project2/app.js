dfcdabde7a8c803504ee05a38ce6707d1f6eb4c3
Harshali Patil <harshali.patil@mindbowser.com>
2026-03-26 11:23:10 +0530

    Assignment7

--- Day1/Practice/Project2/app.js
+++ Day1/Practice/Project2/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
+const path=require('path')
 
 const adminRoutes= require('./routes/admin')
 const shopRoutes= require('./routes/shop')
 // Built-in body parser 
 app.use(express.urlencoded({ extended: true }));
-app.use(adminRoutes)
+//static middleware
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
-
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
--- Day1/Practice/Project2/routes/admin.js
+++ Day1/Practice/Project2/routes/admin.js
@@ -1,19 +1,16 @@
 const express=require('express')
 const router=express.Router()
 
+const path=require('path')
+const rootDir=require('../util/path')
 
 // Form page
 router.get('/add-product', (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title">
-      <button type="submit">Add Product</button>
-    </form>
-  `);
+  res.sendFile(path.join(__dirname,'..','views','add-product.html'))
 });
 
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);  
   res.redirect('/');
 });
