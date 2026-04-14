568eb2299e08d08e26c2a24eb037952e6c9277a5
Harshali Patil <harshali.patil@mindbowser.com>
2026-04-14 17:13:41 +0530

    feat:orders functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-
+const bodyParser=require('body-parser')
+const mongoose=require('mongoose')
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
-const mongoConnect=require('./util/database').mongoConnect;
+//const mongoConnect=require('./util/database').mongoConnect;
 const User=require('./models/user')
 
 app.use(express.urlencoded({ extended: false }));
@@ -18,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req,res,next)=>{
   User.findById("69d7dd43d229035706ae1468")
   .then(user=>{
-    req.user=new User(user.name,user.email,user.cart,user._id)
+    req.user=user
     next()
   })
   .catch(err=>console.log(err))
@@ -31,7 +32,25 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
+
+mongoose.connect('mongodb+srv://harshalipatil_db_user:harshali13@cluster0.qbtt7ft.mongodb.net/shop?retryWrites=true&w=majority').then(result=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user=new User(
+    {
+      name:'Max',
+      email:"max@test.com",
+      cart:{
+        items:[]
+      }
+    }
+  )
+  user.save()
+    }
+  })
   
   app.listen(3000)
+})
+.catch(err=>{
+  console.log(err)
 })
