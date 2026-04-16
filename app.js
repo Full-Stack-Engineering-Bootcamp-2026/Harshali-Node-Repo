2cb3635ec6d4c6af6102a2b2591c697796c116e3
Harshali Patil <harshali.patil@mindbowser.com>
2026-04-16 07:22:04 +0530

    feat:Session and Cookies

--- app.js
+++ app.js
@@ -1,56 +1,76 @@
-const express = require("express");
-const path = require("path");
-const bodyParser=require('body-parser')
-const mongoose=require('mongoose')
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://harshalipatil_db_user:harshali13@cluster0.qbtt7ft.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
-//const mongoConnect=require('./util/database').mongoConnect;
-const User=require('./models/user')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-app.use((req,res,next)=>{
-  User.findById("69d7dd43d229035706ae1468")
-  .then(user=>{
-    req.user=user
-    next()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
   })
-  .catch(err=>console.log(err))
-  
-})
+);
 
-//app.use(productRoutes);
-app.use("/admin", adminRoutes);
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-
-mongoose.connect('mongodb+srv://harshalipatil_db_user:harshali13@cluster0.qbtt7ft.mongodb.net/shop?retryWrites=true&w=majority').then(result=>{
-  User.findOne().then(user=>{
-    if(!user){
-      const user=new User(
-    {
-      name:'Max',
-      email:"max@test.com",
-      cart:{
-        items:[]
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
       }
-    }
-  )
-  user.save()
-    }
+    });
+    app.listen(3000);
   })
-  
-  app.listen(3000)
-})
-.catch(err=>{
-  console.log(err)
-})
+  .catch(err => {
+    console.log(err);
+  });
