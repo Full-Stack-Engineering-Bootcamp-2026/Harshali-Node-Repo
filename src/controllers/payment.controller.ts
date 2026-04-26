9364cc8d3fe96c1ededbf5e9fc9d25cb6ec3d1f3
Harshali Patil <harshali.patil@mindbowser.com>
2026-04-26 08:56:27 +0530

    payment controller modified

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,51 +1,54 @@
-import { Request, Response, NextFunction } from "express"
-import crypto from "crypto"
+import { Request, Response, NextFunction } from "express";
+import crypto from "crypto";
 
-import { createOrderService, updatePaymentService} from "../services/payment.service"
+import {
+  createOrderService,
+  updatePaymentService,
+} from "../services/payment.service";
 
-export const createOrder = async (req: Request, res: Response, next: NextFunction) => {
-
+export const createOrder = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-
-    const { amount } = req.body
-    const order = await createOrderService(amount)
-    res.json(order)
-  } 
-  catch (err) {
-    next(err)
+    const { amount } = req.body;
+    const order = await createOrderService(amount);
+    res.json(order);
+  } catch (err) {
+    next(err);
   }
-}
+};
 
 //verify payement
 
-export const verifyPayment = async (req: Request, res: Response, next: NextFunction) => {
-
+export const verifyPayment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
 
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
-
-    const body = razorpay_order_id + "|" + razorpay_payment_id
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
 
     const expected = crypto
       .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET as string)
       .update(body)
-      .digest("hex")
+      .digest("hex");
 
     if (expected === razorpay_signature) {
       await updatePaymentService(
-
         razorpay_order_id,
         razorpay_payment_id,
-        "success"
-
-      )
-      return res.json({ success: true })
+        "success",
+      );
+      return res.json({ success: true });
     }
 
-    res.status(400).json({ success: false })
-  } 
-  
-  catch (err) {
-    next(err)
+    res.status(400).json({ success: false });
+  } catch (err) {
+    next(err);
   }
-}
+};
